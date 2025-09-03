a6c909dabccb84c22711a7835fc3e7ccf0c4dab8
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-03 00:33:42 -0500

    Update templateRoutes.js

--- routes/templateRoutes.js
+++ routes/templateRoutes.js
@@ -1,1 +1,18 @@
+import express from 'express';
+import {
+  getTemplates,
+  createTemplate,
+  getTemplateById,
+  updateTemplate,
+  deleteTemplate,
+} from '../controllers/templateController.js';
 
+const router = express.Router();
+
+router.get('/', getTemplates);
+router.post('/', createTemplate);
+router.get('/:id', getTemplateById); // This is not required by the prompt, but it's good practice.
+router.put('/:id', updateTemplate);
+router.delete('/:id', deleteTemplate);
+
+export default router;
