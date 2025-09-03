d7d7217132e4a52dc7b23d970b3c4ba077c36231
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-03 00:30:46 -0500

    Update Template.js

--- models/Template.js
+++ models/Template.js
@@ -1,1 +1,28 @@
+import mongoose from 'mongoose';
 
+const templateSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  labels: {
+    type: [String],
+    default: [],
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Template = mongoose.model('Template', templateSchema);
+
+export default Template;
